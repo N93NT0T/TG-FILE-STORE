a069920bcc61e8555ba519b6807bbf723d7835fe
N93NT0T <75997866+N93NT0T@users.noreply.github.com>
2022-03-08 18:54:06 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -84,16 +84,14 @@ async def start_command(client: Bot, message: Message):
         await temp_msg.delete()
 
         for msg in messages:
-           if bool(CUSTOM_CAPTION) & bool(msg.document):
+
+            if bool(CUSTOM_CAPTION) & bool(msg.document):
                 caption = CUSTOM_CAPTION.format(
                     previouscaption="" if not msg.caption else msg.caption.html,
                     filename=msg.document.file_name,
                 )
             else:
-                 caption = "" if not msg.caption else msg.caption.html
-
-            
-                
+                caption = "" if not msg.caption else msg.caption.html
 
             reply_markup = msg.reply_markup if DISABLE_CHANNEL_BUTTON else None
             try:
