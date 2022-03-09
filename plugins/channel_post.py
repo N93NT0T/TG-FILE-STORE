b099759a5332e601ac45d5d7e9af8e97363158d7
N93NT0T <75997866+N93NT0T@users.noreply.github.com>
2022-03-09 16:17:28 +0700

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -9,11 +9,17 @@ from pyrogram.errors import FloodWait
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
 from bot import Bot
-from config import*
+from config import ADMINS, CHANNEL_ID, DISABLE_CHANNEL_BUTTON
 from helper_func import encode
 
 
-@Bot.on_message(    filters.private    & filters.user(ADMINS)    & ~filters.command(        ["start", "users", "broadcast", "ping", "uptime", "batch", "genlink"]    ))
+@Bot.on_message(
+    filters.private
+    & filters.user(ADMINS)
+    & ~filters.command(
+        ["start", "users", "broadcast", "ping", "uptime", "batch", "genlink"]
+    )
+)
 async def channel_post(client: Client, message: Message):
     reply_text = await message.reply_text("<code>Tunggu Sebentar...</code>", quote=True)
     try:
@@ -49,10 +55,33 @@ async def channel_post(client: Client, message: Message):
         reply_markup=reply_markup,
         disable_web_page_preview=True,
     )
-    if not DISABLE_CHANNEL_BUTTON:
-        await post_message.edit(
-        f"{link}",
-        
-        disable_web_page_preview=True,
-        )
 
+    if not DISABLE_CHANNEL_BUTTON:
+        await post_message.edit_reply_markup(reply_markup)
+
+
+@Bot.on_message(
+    filters.channel & filters.incoming & filters.chat(CHANNEL_ID) & ~filters.edited
+)
+async def new_post(client: Client, message: Message):
+
+    if DISABLE_CHANNEL_BUTTON:
+        return
+
+    converted_id = message.message_id * abs(client.db_channel.id)
+    string = f"get-{converted_id}"
+    base64_string = await encode(string)
+    link = f"https://t.me/{client.username}?start={base64_string}"
+    reply_markup = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    "🔁 Share Link", url=f"https://telegram.me/share/url?url={link}"
+                )
+            ]
+        ]
+    )
+    try:
+        await message.edit_reply_markup(reply_markup)
+    except Exception as e:
+        print(e)
