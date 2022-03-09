a7ef7ae0ca2aea637018bc8bf1175354fe2aedfa
N93NT0T <75997866+N93NT0T@users.noreply.github.com>
2022-03-09 19:05:43 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -12,19 +12,29 @@ from config import CHANNEL, GROUP, OWNER
 async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
-        await query.message.edit_text(
-            text=f"<b>Tentang Bot ini:\n\n • Owner: @{OWNER}\n • Channel: @{CHANNEL}\n • Group: @{GROUP}\n • Source Code: <a href='https://github.com/mrismanaziz/File-Sharing-Man'>Klik Disini</a>\n • Owner Repo: @mrismanaziz</b>\n",
-            disable_web_page_preview=True,
+        await query.message.reply_photo(
+            photo=CAPTION_ABOUT_PHOTO,
+            caption=CAPTION_ABOUT, 
+            disable_web_page_preview=True, 
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("• ᴛᴜᴛᴜᴘ •", callback_data="close")]]
-            ),
+            ),protect_content=True, 
+            parse_mode='html'
+        )
+            
         )
 
             
     elif data == "close":
         await query.message.delete()
         await query.message.reply_photo(
-            photo="https://telegra.ph/file/7c5c0dc8ee6723aac16be.jpg",
-            
+            photo=CAPTION_ABOUT_PHOTO,
+            caption=CAPTION_CLOSE, 
+            parse_mode='html', 
+            protect_content=True,
+            disable_web_page_preview=True, 
         )
-        
+        try:
+            await query.message.reply_to_message.delete()
+        except BaseException:
+            pass
