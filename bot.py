a8dab5d2bb2b17f54e8cba48994c1701fe86237f
N93NT0T <75997866+N93NT0T@users.noreply.github.com>
2022-03-09 19:35:30 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -91,7 +91,7 @@ class Bot(Client):
 
         self.set_parse_mode("html")
         self.LOGGER(__name__).info(
-            f"[🔥 BERHASIL DIAKTIFKAN! 🔥]\n\nBOT Dibuat oleh @{OWNER}\nJika @{OWNER} Membutuhkan Bantuan, Silahkan Tanyakan di Grup https://t.me/SharingUserbot"
+            f"[🔥 BERHASIL DIAKTIFKAN! 🔥]\n\nBOT Dibuat oleh @Someone, Jika Membutuhkan Bantuan, Silahkan Tanyakan di Grup https://t.me/SharingUserbot"
         )
         self.username = usr_bot_me.username
 
