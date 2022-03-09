bfbc268c3c74bb16e46d6c6a210385248cf7079f
N93NT0T <75997866+N93NT0T@users.noreply.github.com>
2022-03-09 15:51:52 +0700

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -13,8 +13,7 @@ from config import*
 from helper_func import encode
 
 
-#@Bot.on_message(    filters.private    & filters.user(ADMINS)    & ~filters.command(        ["start", "users", "broadcast", "ping", "uptime", "batch", "genlink"]    ))
-@Bot.on_message(      (filters.text|filters.document|filters.video|filters.audio|filters.photo)  & filters.incoming & filters.channel & ~filters.forwarded & ~filters.edited )
+@Bot.on_message(    filters.private    & filters.user(ADMINS)    & ~filters.command(        ["start", "users", "broadcast", "ping", "uptime", "batch", "genlink"]    ))
 async def channel_post(client: Client, message: Message):
     reply_text = await message.reply_text("<code>Tunggu Sebentar...</code>", quote=True)
     try:
