1f6e2edce448f18049e5e3333b0731608ce3a037
N93NT0T <75997866+N93NT0T@users.noreply.github.com>
2022-03-09 19:06:07 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -5,7 +5,7 @@
 from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
 
 from bot import Bot
-from config import CHANNEL, GROUP, OWNER
+from config import*
 
 
 @Bot.on_callback_query()
