3aa0747b273c3ba59396919f48b4dcc83d089e2e
N93NT0T <75997866+N93NT0T@users.noreply.github.com>
2022-03-09 17:46:35 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -5,19 +5,25 @@
 from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
 
 from bot import Bot
-from config import CHANNEL, GROUP, OWNER
+from config import CHANNEL, GROUP, OWNER, START_MESSAGE
 
 
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
-        await query.edit_message_media(
-            media="https://telegra.ph/file/7c5c0dc8ee6723aac16be.jpg", 
-            reply_markup=InlineKeyboardMarkup(
+        await Bot.send_photo(chat_id="me",
+        photo="https://telegra.ph/file/7c5c0dc8ee6723aac16be.jpg",
+        caption=START_MESSAGE,
+        reply_markup=reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("• ᴛᴜᴛᴜᴘ •", callback_data="close")]]
             ),
-        )
+        parse_mode="HTML",
+        disable_notification=True,
+        protect_content=True
+    )
+
+            
     elif data == "close":
         await query.message.delete()
         try:
